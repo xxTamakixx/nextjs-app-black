7bfebd21baeb5ba1f5172b6cfda5fdb0b6b78bba
xxJihNxx <tamatomo0116@gmail.com>
2023-07-14 19:31:58 +0900

    edit page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
+      <div>xxxTestxxx</div>
       <div>
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
